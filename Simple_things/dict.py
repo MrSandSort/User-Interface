e2c9d98964898b49916c0855121fcf07541a02e7
Sandesh Paudel <114731627+MrSandSort@users.noreply.github.com>
2022-09-30 16:38:22 +0545

    Add files via upload

---
+++ Simple_things/dict.py
@@ -0,0 +1,16 @@
+
+x={1:['Football','12','Ronaldo'],2:['Cricket','49','Virat'],3:['Volleyball','65','Ranjit']}
+z=list(x.values())
+
+a=z[0]
+b=z[1]
+c=z[2]
+
+for i in range(1):
+    print(a[i],a[i+1],a[i+2])
+    print(b[i],b[i+1],b[i+2])
+    print(c[i],c[i+1],c[i+2])
+    
+
+    
+    
