e03a4ecbc839777dd20581b4ea106b07938243a1
Sandesh Paudel <114731627+MrSandSort@users.noreply.github.com>
2022-09-30 16:36:53 +0545

    Add files via upload

---
+++ Simple_things/sum.py
@@ -0,0 +1,47 @@
+'''
+iter=[]
+
+iter.append(1)
+iter.append(2)
+iter.append(3)
+iter.append(4)
+iter.append(5)
+
+print(iter)
+
+iter=[8,9,1,4,7,6]
+iter[0]=2
+iter[1]=8
+
+iter.sort()
+iter.reverse()
+
+
+print(iter)
+
+'''
+
+# printing 1 to 20 numbers
+'''
+num= list(range(1,21))
+print(num)
+
+x= list(range(1,20,2))
+print(x)
+
+'''
+
+
+# print multiples of 3 from 3 to 30:
+
+'''
+x= list(range(3,31,3))
+print(x)
+
+'''
+
+z=[hand*3 for hand in range(1,11)]
+print(z)
+
+
+
